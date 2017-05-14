110357f8eeba2f138bf97b053eb924004a5e2252
Newthrone <kent682@mail.ru>
2017-05-14 22:45:21 +0300

    Загрузил файлы
    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
    <script>window.JQuery || document.write('<script src="js/jquery-3.1.1.min.js"></script>');</script>

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+
+<input name="email" placeholder="Email"><br>
+<input name="name" placeholder="Имя"><br>
+<input name="phone" placeholder="Телефон"><br>
+<textarea name="message" placeholder="Сообщение"></textarea><br>
+<input type="button" value="Отправить сообщение">
+<div class='result'></div>
+
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script>window.JQuery || document.write('<script src="js/jquery-3.1.1.min.js"><\/script>');</script>
+<script src='js/script.js'></script>
+
+</body>
+</html>
